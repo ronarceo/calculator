454919fc52411e206145c3a3695a3492a2401ac1
ronarceo <ronaldmarceo@gmail.com>
2022-03-22 21:41:40 -0500

    number buttons update screen on ui, clear and delete buttons working

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('test')
+const numberBtns = document.querySelectorAll('[data-number]');
+const currentOperationScreen = document.querySelector('#currentOperationScreen');
+const clearBtn = document.querySelector('#clearBtn');
+const deleteBtn = document.querySelector('#deleteBtn');
+
+numberBtns.forEach(numberBtn => {
+    numberBtn.addEventListener('click', () => updateNumber(numberBtn.textContent));
+})
+
+function updateNumber(number) {
+    if (currentOperationScreen.textContent === '0') {
+        currentOperationScreen.textContent = '';
+    }
+    currentOperationScreen.textContent += number;
+}
+
+clearBtn.addEventListener('click', clearScreen);
+
+function clearScreen() {
+    currentOperationScreen.textContent = '0';
+}
+
+deleteBtn.addEventListener('click', deleteNumber);
+
+function deleteNumber() {
+    currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
+}
